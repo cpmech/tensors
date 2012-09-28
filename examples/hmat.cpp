38ae6e5ceefca37e2f1d99ad78bf6ab07ccbb90a
Dorival Pedroso <dorival.pedroso@gmail.com>
2012-09-28 17:52:29 +1000

    hmat added

---
+++ examples/hmat.cpp
@@ -0,0 +1,87 @@
+/*************************************************************************************
+ * Tensors - A C++ library to work with tensors                                      *
+ * Copyright (C) 2004 Dorival de Moraes Pedroso <dorival.pedroso em gmail.com>       *
+ *                                                                                   *
+ * This file is part of Tensors.                                                     *
+ *                                                                                   *
+ * Tensors is free software; you can redistribute it and/or modify it under the      *
+ * terms of the GNU General Public License as published by the Free Software         *
+ * Foundation; either version 2 of the License, or (at your option) any later        *
+ * version.                                                                          *
+ *                                                                                   *
+ * Tensors is distributed in the hope that it will be useful, but WITHOUT ANY        *
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A   *
+ * PARTICULAR PURPOSE. See the GNU General Public License for more details.          *
+ *                                                                                   *
+ * You should have received a copy of the GNU General Public License along with      *
+ * Tensors; if not, write to the Free Software Foundation, Inc., 51 Franklin Street, *
+ * Fifth Floor, Boston, MA 02110-1301, USA                                           *
+ *************************************************************************************/
+
+#include <iostream>
+#include <iomanip>
+#include <sstream>
+#include <string>
+#include <ginac/ginac.h>
+
+#include <tensors/tensor1.h>
+#include <tensors/tensor2.h>
+#include <tensors/tensor3.h>
+#include <tensors/tensor4.h>
+#include <tensors/operators.h>
+
+typedef GiNaC::symbol _S;
+
+using namespace std;
+using namespace TensorsLib;
+
+int main(int argc, char **argv)
+{
+    // input arguments
+    bool is2d = false;
+    if (argc>1) is2d = atoi(argv[1]);
+
+    // ndim
+    int ndim = (is2d ? 2 : 3);
+
+    // Gm = dSmdx
+    Tensor1<GiNaC::ex,3> Gm, Gn;
+    if (ndim==2) {
+        Gm = _S("Gm0"), _S("Gm1"), _S("0");
+        Gn = _S("Gn0"), _S("Gn1"), _S("0");
+    } else {
+        Gm = _S("Gm0"), _S("Gm1"), _S("Gm2");
+        Gn = _S("Gn0"), _S("Gn1"), _S("Gn2");
+    }
+
+    // kap
+    Tensor2<GiNaC::ex,3> kap;
+    for (size_t i=0; i<3; ++i) {
+        for (size_t j=0; j<3; ++j) {
+            std::ostringstream oss;
+            oss << "k" << i << j;
+            kap[i][j] = _S(oss.str());
+        }
+    }
+
+    // GmKGn
+    GiNaC::ex res;
+    for (int i=0; i<ndim; ++i) {
+        for (int j=0; j<ndim; ++j) {
+            res += Gm[i] * kap[i][j] * Gn[j];
+        }
+    }
+    cout << "Hmn:" << res << ", ratsimp;\n";
+
+    // f90
+    if (ndim==2) {
+        cout << "\nload(f90);\nwith_stdout(\"hmat2d.f90\",";
+    } else {
+        cout << "\nload(f90);\nwith_stdout(\"hmat3d.f90\",";
+    }
+    cout << "f90('Hmn=Hmn)";
+    cout << ");\n";
+
+    // end
+	return 0;
+}
---
+++ examples/matmatmul.cpp
@@ -0,0 +1,73 @@
+/*************************************************************************************
+ * Tensors - A C++ library to work with tensors                                      *
+ * Copyright (C) 2004 Dorival de Moraes Pedroso <dorival.pedroso em gmail.com>       *
+ *                                                                                   *
+ * This file is part of Tensors.                                                     *
+ *                                                                                   *
+ * Tensors is free software; you can redistribute it and/or modify it under the      *
+ * terms of the GNU General Public License as published by the Free Software         *
+ * Foundation; either version 2 of the License, or (at your option) any later        *
+ * version.                                                                          *
+ *                                                                                   *
+ * Tensors is distributed in the hope that it will be useful, but WITHOUT ANY        *
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A   *
+ * PARTICULAR PURPOSE. See the GNU General Public License for more details.          *
+ *                                                                                   *
+ * You should have received a copy of the GNU General Public License along with      *
+ * Tensors; if not, write to the Free Software Foundation, Inc., 51 Franklin Street, *
+ * Fifth Floor, Boston, MA 02110-1301, USA                                           *
+ *************************************************************************************/
+
+#include <iostream>
+#include <iomanip>
+#include <sstream>
+#include <string>
+#include <ginac/ginac.h>
+
+#include <tensors/tensor1.h>
+#include <tensors/tensor2.h>
+#include <tensors/tensor3.h>
+#include <tensors/tensor4.h>
+#include <tensors/operators.h>
+
+typedef GiNaC::symbol _S;
+
+using namespace std;
+using namespace TensorsLib;
+
+int main(int argc, char **argv)
+{
+    // identity and D tensor
+    const size_t n = 10;
+    Tensor2<GiNaC::ex,n> A, B, C;
+
+    for (size_t i=0; i<n; ++i) {
+        for (size_t j=0; j<n; ++j) {
+            std::ostringstream oa, ob, oc;
+            oa << "a" << i << j;
+            ob << "b" << i << j;
+            //oc << "c" << i << j;
+            A[i][j] = _S(oa.str());
+            B[i][j] = _S(ob.str());
+            //C[i][j] = _S(oc.str());
+        }
+    }
+
+    cout << "A =\n" << A << endl;
+    cout << "B =\n" << B << endl;
+
+    for (size_t i=0; i<n; ++i) {
+        for (size_t j=0; j<n; ++j) {
+            C[i][j] = 0;
+            for (size_t k=0; k<n; ++k) {
+                C[i][j] += A[i][k] * B[k][j];
+            }
+        }
+    }
+
+    C.SetMaxima();
+    cout << "C =\n" << C << endl;
+
+    // end
+	return 0;
+}
